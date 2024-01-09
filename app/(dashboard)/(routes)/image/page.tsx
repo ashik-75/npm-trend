9d7f3855f765ae3fe5b5c8770dafec4e84481296
ashik-75 <ashikex49@gmail.com>
2024-01-09 13:34:07 +0600

    fix:remove dashboard

--- app/(dashboard)/(routes)/image/page.tsx
+++
@@ -1,5 +0,0 @@
-const Image = () => {
-  return <div>Image generation</div>;
-};
-
-export default Image;
--- app/(dashboard)/(routes)/video/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const VideoGeneration = () => {
-  return <div>VideoGeneration</div>;
-};
-
-export default VideoGeneration;
